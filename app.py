a2cb50e02cf194b6b61940d50b1426e1a71ad460
Elif Zeynep Elverisli <109090494+ezeynepel@users.noreply.github.com>
2025-05-18 18:00:53 +0300

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,8 @@ model, selected_features = load_model()
 # Title and intro
 st.title("Term Deposit Subscription Prediction")
 st.markdown("""
-Use the form below to enter client information and predict whether the client will subscribe to a **bank term deposit**.
+Use the form below to enter client information and predict whether the client will subscribe to a
+**bank term deposit**.
 """)
 
 # Form
