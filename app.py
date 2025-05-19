05b504cfa482a382d8775c273270aed046ef4387
Elif Zeynep Elverisli <109090494+ezeynepel@users.noreply.github.com>
2025-05-19 10:59:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ input_data = pd.DataFrame([{
 }])
 
 
-gap1, center, gap2 = st.columns([2, 1, 1])
+gap1, center, gap2 = st.columns([2, 1, 2])
 with center:
     predict_button = st.button("Predict")
 
